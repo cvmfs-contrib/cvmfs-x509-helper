ae055c200ca501196e3dacfe3c0931d03f25e517
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2021-08-20 15:19:30 -0500

    close scitoken file after use

--- src/x509_helper.cc
+++ src/x509_helper.cc
@@ -214,6 +214,7 @@ int main(int argc, char **argv) {
         StatusSciTokenValidation validation_status =
           (*checker)(request.membership.c_str(), fp_token, fp_debug);
         LogAuthz(kLogAuthzDebug, "validation status is %d", validation_status);
+        fclose(fp_token);
 
         if (validation_status == kCheckTokenGood) {
           WriteMsg("{\"cvmfs_authz_v1\":{\"msgid\":3,\"revision\":0,"
